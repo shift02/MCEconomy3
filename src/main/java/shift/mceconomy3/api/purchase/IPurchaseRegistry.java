f62691af86475c99ab718504804103063e44a9d6
shift02 <shift02ss@gmail.com>
2017-10-23 22:08:02 +0900

    追加：液体の登録処理

--- src/main/java/shift/mceconomy3/api/purchase/IPurchaseRegistry.java
+++ src/main/java/shift/mceconomy3/api/purchase/IPurchaseRegistry.java
@@ -4,6 +4,8 @@ import net.minecraft.item.ItemStack;
 
 public interface IPurchaseRegistry  {
 
+	public void addPurchase(IPurchase purchase);
+
 	/**
 	 * addPurchaseItem アイテムの買い取り価格を判定するクラスを登録
 	 * @param purchaseItem 登録するIPurchaseItemクラス
@@ -23,5 +25,15 @@ public interface IPurchaseRegistry  {
 	 * @param amount 価格  -1で非売品に設定出来ます
 	 */
 	public void addPurchaseItem(String oreName, int amount);
+
+
+	/**
+	 * addPurchaseItem 液体の買い取り価格を判定するクラスを登録
+	 * @param purchaseItem 登録するIPurchaseFluidクラス
+	 */
+	public void addPurchaseFluid(IPurchaseFluid purchaseFluid);
+
+	public int getPurchase(Object object);
+
 
 }
--- src/main/java/shift/mceconomy3/purchase/PurchaseRegistry.java
+++ src/main/java/shift/mceconomy3/purchase/PurchaseRegistry.java
@@ -2,6 +2,8 @@ package shift.mceconomy3.purchase;
 
 import net.minecraft.item.ItemStack;
 import shift.mceconomy3.api.MCEconomyAPI;
+import shift.mceconomy3.api.purchase.IPurchase;
+import shift.mceconomy3.api.purchase.IPurchaseFluid;
 import shift.mceconomy3.api.purchase.IPurchaseItem;
 import shift.mceconomy3.api.purchase.IPurchaseRegistry;
 
@@ -25,6 +27,23 @@ public class PurchaseRegistry implements IPurchaseRegistry {
 	public void addPurchaseItem(String oreName, int amount) {
 
 		MCEconomyAPI.addPurchaseItem(oreName, amount);
+
+	}
+
+	@Override
+	public void addPurchase(IPurchase purchase) {
+
+	}
+
+	@Override
+	public int getPurchase(Object object) {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
+	@Override
+	public void addPurchaseFluid(IPurchaseFluid purchaseFluid) {
+		// TODO 自動生成されたメソッド・スタブ
 
 	}
 
