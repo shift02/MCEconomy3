ffee38addbe8564d15f293ecfaeb82ba71fdd2ca
shift02 <shift02ss@gmail.com>
2017-10-25 23:06:44 +0900

    追加：登録処理

--- src/main/java/shift/mceconomy3/api/purchase/IPurchaseRegistry.java
+++ src/main/java/shift/mceconomy3/api/purchase/IPurchaseRegistry.java
@@ -2,38 +2,36 @@ package shift.mceconomy3.api.purchase;
 
 import net.minecraft.item.ItemStack;
 
-public interface IPurchaseRegistry  {
+public interface IPurchaseRegistry {
 
-	public void addPurchase(IPurchase purchase);
+    public <T> void addPurchase(IPurchase<T> purchase, Class<T> purchaseClass);
 
-	/**
-	 * addPurchaseItem アイテムの買い取り価格を判定するクラスを登録
-	 * @param purchaseItem 登録するIPurchaseItemクラス
-	 */
-	public void addPurchaseItem(IPurchaseItem purchaseItem);
+    /**
+     * addPurchaseItem アイテムの買い取り価格を判定するクラスを登録
+     * @param purchaseItem 登録するIPurchaseItemクラス
+     */
+    public void addPurchaseItem(IPurchaseItem purchaseItem);
 
-	/**
-	 * addPurchaseItem Mobなどがアイテムを買い取る価格をItemStackで登録
-	 * @param PurchaseItem 買い取りアイテム
-	 * @param amount 価格  -1で非売品に設定出来ます
-	 */
-	public void addPurchaseItem(ItemStack PurchaseItem, int amount);
+    /**
+     * addPurchaseItem Mobなどがアイテムを買い取る価格をItemStackで登録
+     * @param PurchaseItem 買い取りアイテム
+     * @param amount 価格  -1で非売品に設定出来ます
+     */
+    public void addPurchaseItem(ItemStack PurchaseItem, int amount);
 
-	/**
-	 * addPurchaseItem Mobなどがアイテムを買い取る価格を鉱石辞書名で登録
-	 * @param oreName 買い取り対象となる鉱石辞書名
-	 * @param amount 価格  -1で非売品に設定出来ます
-	 */
-	public void addPurchaseItem(String oreName, int amount);
+    /**
+     * addPurchaseItem Mobなどがアイテムを買い取る価格を鉱石辞書名で登録
+     * @param oreName 買い取り対象となる鉱石辞書名
+     * @param amount 価格  -1で非売品に設定出来ます
+     */
+    public void addPurchaseItem(String oreName, int amount);
 
+    /**
+     * addPurchaseItem 液体の買い取り価格を判定するクラスを登録
+     * @param purchaseItem 登録するIPurchaseFluidクラス
+     */
+    public void addPurchaseFluid(IPurchaseFluid purchaseFluid);
 
-	/**
-	 * addPurchaseItem 液体の買い取り価格を判定するクラスを登録
-	 * @param purchaseItem 登録するIPurchaseFluidクラス
-	 */
-	public void addPurchaseFluid(IPurchaseFluid purchaseFluid);
-
-	public int getPurchase(Object object);
-
+    public int getPurchase(Object object);
 
 }
--- src/main/java/shift/mceconomy3/purchase/PurchaseRegistry.java
+++ src/main/java/shift/mceconomy3/purchase/PurchaseRegistry.java
@@ -1,5 +1,8 @@
 package shift.mceconomy3.purchase;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
 import net.minecraft.item.ItemStack;
 import shift.mceconomy3.api.MCEconomyAPI;
 import shift.mceconomy3.api.purchase.IPurchase;
@@ -9,42 +12,46 @@ import shift.mceconomy3.api.purchase.IPurchaseRegistry;
 
 public class PurchaseRegistry implements IPurchaseRegistry {
 
-	@Override
-	public void addPurchaseItem(IPurchaseItem purchaseItem) {
+    public Multimap<Class, IPurchase> purchases = ArrayListMultimap.create();
 
-		MCEconomyAPI.addPurchaseItem(purchaseItem);
+    @Override
+    public void addPurchaseItem(IPurchaseItem purchaseItem) {
 
-	}
+        MCEconomyAPI.addPurchaseItem(purchaseItem);
 
-	@Override
-	public void addPurchaseItem(ItemStack PurchaseItem, int amount) {
+    }
 
-		MCEconomyAPI.addPurchaseItem(PurchaseItem, amount);
+    @Override
+    public void addPurchaseItem(ItemStack PurchaseItem, int amount) {
 
-	}
+        MCEconomyAPI.addPurchaseItem(PurchaseItem, amount);
 
-	@Override
-	public void addPurchaseItem(String oreName, int amount) {
+    }
 
-		MCEconomyAPI.addPurchaseItem(oreName, amount);
+    @Override
+    public void addPurchaseItem(String oreName, int amount) {
 
-	}
+        MCEconomyAPI.addPurchaseItem(oreName, amount);
 
-	@Override
-	public void addPurchase(IPurchase purchase) {
+    }
 
-	}
+    @Override
+    public <T> void addPurchase(IPurchase<T> purchase, Class<T> purchaseClass) {
 
-	@Override
-	public int getPurchase(Object object) {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
-	}
+        purchases.put(purchaseClass, purchase);
 
-	@Override
-	public void addPurchaseFluid(IPurchaseFluid purchaseFluid) {
-		// TODO 自動生成されたメソッド・スタブ
+    }
 
-	}
+    @Override
+    public int getPurchase(Object object) {
+        // TODO 自動生成されたメソッド・スタブ
+        return 0;
+    }
+
+    @Override
+    public void addPurchaseFluid(IPurchaseFluid purchaseFluid) {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
 
 }
