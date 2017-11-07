7888a5f106ba0e0c97a6295101ccd906b1eeec89
shift02 <shift02ss@gmail.com>
2017-11-07 21:57:33 +0900

    更新：バージョン

--- src/main/java/shift/mceconomy3/MCEconomy3.java
+++ src/main/java/shift/mceconomy3/MCEconomy3.java
@@ -27,7 +27,7 @@ import shift.mceconomy3.purchase.PurchaseManager;
 public class MCEconomy3 {
 
     public static final String MODID = "mceconomy3";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.1.0";
 
     public static final String UPDATE_JSON = "https://shift02.github.io/MCEconomy3/Update.json";
 
